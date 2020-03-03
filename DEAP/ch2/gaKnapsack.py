862cb50131de5bb2ccc27d9185e3de5aab9c996b
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-03 15:39:47 +0000

    Constants
    Setting up constants to be used in the program

--- DEAP/ch2/gaKnapsack.py
+++ DEAP/ch2/gaKnapsack.py
@@ -10,3 +10,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import knapsack
+
+# problem constants:
+# Using the problem class defined in knapsack.py
+
+knapsack= knapsack.Knapsack01Problem()
+
+# Algortihm Constants:
+
+POPULATION_SIZE = 50
+P_CROSSOVER = 0.9 # prob of crossover
+P_MUTATATION = 0.1 # prob of mutation
+MAX_GENERATIONS = 50
+HALL_OF_FAME_SIZE = 1
+
+# Setting the seed so we get the same result each time
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
