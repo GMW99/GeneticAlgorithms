0d497a4e6b7a2d9dc169d0a38dfa2fa92bb1ec74
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-03 15:35:50 +0000

    Imports
    Adding the imports that are going to be used in this program

---
+++ DEAP/ch2/gaKnapsack.py
@@ -0,0 +1,12 @@
+from deap import base
+from deap import creator
+from deap import tools
+from deap import algortihms
+
+import random
+import numpy
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import knapsack
