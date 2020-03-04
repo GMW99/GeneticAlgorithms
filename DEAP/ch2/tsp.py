93e47c6313531b41f7a3710f89c7871250fac873
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-04 13:52:22 +0000

    Outline of class
    This gives an outline of the class with method needing to be properly
    implemented

---
+++ DEAP/ch2/tsp.py
@@ -0,0 +1,30 @@
+import csv
+import pickle
+import os
+import codecs
+
+import numpy as np
+
+from urllib.request import urlopen
+
+import matplotlib.pyplot as plt
+
+class TravellingSalesmanProblem:
+
+    def __init__(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __initData(self):
+        pass
+    
+    def __createData(self):
+        pass
+
+    def getTotalDistance(self):
+        pass
+    
+    def plotData(self):
+        pass
