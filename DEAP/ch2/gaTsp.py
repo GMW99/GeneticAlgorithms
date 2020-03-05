de126bb306a88a0c742891730c13e6e8eeb9679d
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-05 10:29:48 +0000

    Setup
    Entering all the constants that will be used throughout the program

--- DEAP/ch2/gaTsp.py
+++ DEAP/ch2/gaTsp.py
@@ -10,3 +10,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import tsp
+
+# problem constants:
+# Using the problem class defined in tsp.py
+TSP_NAME = "bayg29"
+tsp = tsp.TravelingSalesmanProblem(TSP_NAME)
+
+# Algortihm Constants:
+POPULATION_SIZE = 300
+P_CROSSOVER = 0.9 # prob of crossover
+P_MUTATATION = 0.1 # prob of mutation
+MAX_GENERATIONS = 200
+HALL_OF_FAME_SIZE = 1
+
+# Setting the seed so we get the same result each time
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
+
+# toolbox set up
+
+toolbox = base.Toolbox()
+
