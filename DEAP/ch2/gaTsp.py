fc51119e387a8d50cb432e2e3c800ce500ce15b4
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-05 10:26:15 +0000

    Imports
    The modules that are going to be used

---
+++ DEAP/ch2/gaTsp.py
@@ -0,0 +1,12 @@
+from deap import base
+from deap import creator
+from deap import tools
+from deap import algorithms
+
+import random
+import numpy
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import tsp
