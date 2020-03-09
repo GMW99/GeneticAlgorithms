78508a1f553250471c16654c57ba68a19e3458d6
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-09 15:57:59 +0000

    Toolbox setup
    Added all the relevant function so toolbox can be used

--- DEAP/ch3/gaEggholder.py
+++ DEAP/ch3/gaEggholder.py
@@ -28,3 +28,21 @@ CROWDING_FACTOR = 20.0
 # Setting the seed so we get the same result each time
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
+
+toolbox = base.Toolbox()
+
+def randomFloat(low,up,dimention):
+    return [random.uniform(1,u) for 1, u in zip([low]* dimention, [up]*dimention)]
+toolbox.register("floatAttr", randomFloat,BOUND_LOW, BOUND_UP, DIMENTIONS)
+
+# define a single objective, minimizing fitness strategy
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+# create the Individual class based on list
+creator.create("Individual",list, fitness=creator.FitnessMin)
+
+# create the individual operator to fill up an Individual instance
+toolbox.register("individualCreator", tools.initIterate, creator.Individual, toolbox.floatAttr)
+
+# create the population operator to generate a list of individuals
+toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
