63fc6e411f3e925b060132e6f027e6f1f3be8ce5
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-11 11:39:36 +0000

    Added GA Loop
    Change some of the constants so the GA found the right solution

--- DEAP/ch4/gaFriedman.py
+++ DEAP/ch4/gaFriedman.py
@@ -2,12 +2,14 @@ from deap import base
 from deap import creator 
 from deap import tools
 
-import random
+
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 import friedman
 import elitism
-
+import random
 # Problem constants 
 
 NUM_FEATURES = 15
@@ -16,9 +18,9 @@ NUM_SAMPLES = 60
 # Genetic Algorithm constants
 POPULATION_SIZE = 30 # number of individuals in population
 P_CROSSOVER = 0.9 # probability for crossover
-P_MUTATION = 0.1 # probability for mutating an individual
+P_MUTATION = 0.3 # probability for mutating an individual
 MAX_GENERATIONS = 30 # max  number of generations for stopping condition
-HALL_OF_FRAME_SIZE = 3
+HALL_OF_FAME_SIZE = 5
 
 # Setting the seed so we get the same result each time
 RANDOM_SEED = 42
@@ -39,3 +41,44 @@ toolbox.register("evaluate", friedmanFitness)
 toolbox.register("select", tools.selTournament, tournsize=2)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=1.0/len(friedman))
+
+# Main GA loop
+
+def main():
+    # create initial population (generation 0):
+    population = toolbox.populationCreator(n=POPULATION_SIZE)
+
+    # prepare the statistics object:
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("min", np.min)
+    stats.register("avg", np.mean)
+
+    # define the hall-of-fame object:
+    hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+
+    # perform the Genetic Algorithm flow with hof feature added
+    population, logbook = elitism.eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
+                                              ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True)
+
+    # print best individual info
+    best = hof.items[0]
+    print("-- Best Ever Individual = ", best)
+    print("-- Best Ever Fitness = ", best.fitness.values[0])
+
+
+    # plot statistics
+    minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
+    plt.figure(2)
+    sns.set_style("whitegrid")
+    plt.plot(minFitnessValues, color='red', label='minFitness')
+    plt.plot(meanFitnessValues, color='green', label='avgFitness')
+    plt.legend()
+    plt.xlabel('Generation')
+    plt.ylabel('Min / Average Fitness')
+    plt.title('Min and Average fitness over Generations')
+
+    # show both plots
+    plt.show()
+
+if __name__  == "__main__":
+    main()
