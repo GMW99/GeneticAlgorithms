7060cef74628aaf65caf3367accb9edc22bc379b
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2020-03-11 10:07:29 +0000

    imports
    importing the relevenat libaries that will be usd

---
+++ DEAP/ch4/friedman.py
@@ -0,0 +1,12 @@
+#Maths library
+import numpy as np
+
+# Data science library
+from sklearn import model_selection
+from sklearn import datasets
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+# Ploting library
+import matplotlib.pyplot as plt
+import seaborn as sns
